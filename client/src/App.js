bd6035088ee6cab0580b761d54017fc36c6450f1
tdj03001 <tyjenki@gmail.com>
2020-04-24 23:25:24 -0400

    added Search component

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import NavBar from "./components/NavBar/index";
 import Footer from "./components/Footer/index";
+import SearchForm from './components/Search';
 
 export default function App() {
   return (
     <div>
       <NavBar />
+      <SearchForm />
       <Footer />
     </div>
   )
