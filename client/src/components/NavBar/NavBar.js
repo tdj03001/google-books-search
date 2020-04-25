f8822255cb6c0e61c6098d7d2fd149c78f213f1e
tdj03001 <tyjenki@gmail.com>
2020-04-24 21:05:11 -0400

    initial commit of component

---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  }
+};
+
+function NavBar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+            Google Books Search
+          </Typography>
+          <Button href="/" color="inherit">Saved Books</Button>
+          <Button href="/" color="inherit">Search</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default withStyles(styles)(NavBar);
+
