cc607b3f8a483eae0a552bdae2330836ba1b8632
tdj03001 <tyjenki@gmail.com>
2020-04-25 22:20:48 -0400

    refactored App.js from original approach of simple component import. Added new components and Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,39 @@
-import React from 'react';
-import './App.css';
-import NavBar from "./components/NavBar/index";
-import Footer from "./components/Footer/index";
-import SearchForm from './components/Search';
+// import React from 'react';
+// import './App.css';
+// import NavBar from "./components/NavBar/index";
+// import Footer from "./components/Footer/index";
+// import SearchForm from './components/Search';
+
+// export default function App() {
+//   return (
+//     <div>
+//       <NavBar />
+//       <SearchForm />
+//       <Footer />
+//     </div>
+//   )
+// };
+
+
+
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Bookshelf from "./pages/bookshelf";
+import Search from "./pages/search";
+import NavBar from "./components/NavBar/NavBar";
 
 export default function App() {
   return (
-    <div>
-      <NavBar />
-      <SearchForm />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+        <Route exact path="/" component={Search} />
+        <Route exact path="/bookshelf" component={Bookshelf} />
+        <Route exact path="/search" component={Search} />
+
+      </div>
+    </Router>
   )
 };
+
+
